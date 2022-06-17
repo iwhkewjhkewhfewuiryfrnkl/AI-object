6575bf454a1f621ecb02f9c81183d71d0bd9661b
iwhkewjhkewhfewuiryfrnkl <106671896+iwhkewjhkewhfewuiryfrnkl@users.noreply.github.com>
2022-06-17 13:51:30 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+status = "";
+
+function preload(){
+    video = createVideo(VIDEO);
+    video.size(480, 380)
+    video.hide();
+}
+
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    }
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+   
+}
